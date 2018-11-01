80f2fd0d3caa2deb88647d1772e94b3083981ac4
theinfamousAK <akhilkalepu@protonmail.com>
2018-11-01 18:32:53 -0400

    edits

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -16,4 +16,8 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "/../public/home.html"));
     });
+    // No match sends to home
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 };
