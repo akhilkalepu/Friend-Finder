9d06d9be4b69d67f705c5e563a5b5cae34e48659
theinfamousAK <akhilkalepu@protonmail.com>
2018-11-01 16:15:22 -0400

    edits.

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,24 +2,24 @@
 // =============================================================
 var friends = [
     {
-        name: "Charlie",
-        photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+        name: "Kramer",
+        photo: "https://upload.wikimedia.org/wikipedia/en/b/b7/Cosmo_Kramer.jpg",
         scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
     }, {
-        name: "Sweet Dee",
-        photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+        name: "Elaine",
+        photo: "https://upload.wikimedia.org/wikipedia/en/3/33/Elaine-benes-3707.jpg",
         scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
     }, {
-        name: "Dennis",
-        photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+        name: "Jerry",
+        photo: "https://upload.wikimedia.org/wikipedia/en/f/f6/Jerry_Seinfeld.jpg",
         scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
     }, {
-        name: "Mac",
-        photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+        name: "George",
+        photo: "https://upload.wikimedia.org/wikipedia/en/7/70/George_Costanza.jpg",
         scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
     }, {
-        name: "Frank",
-        photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+        name: "Newman",
+        photo: "https://upload.wikimedia.org/wikipedia/en/8/8c/Newman_Seinfeld.jpg",
         scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
     }
 ];
